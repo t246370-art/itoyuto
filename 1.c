fb7157d772b6687a9e066de2f2faa4434fba0854
t246370-art <t246370@st.yamagata-u.ac.jp>
2025-10-10 19:01:47 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+/*あいうえお*/
+
+#include <stdio.h>
+
+int main()
+{
+    int banzai = 8931;//名前を付けて数字を保存
+    printf("はくさいが%d個です\n",banzai);//printfは""の間に表記したい文字を書く
+    banzai = 8931*4;
+    printf("はくさいが%d個です\n",banzai);
+
+    return 0;
+}
