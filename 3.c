f1d62017d1fb278795d3930ff5bbfe9052508c9b
t246370-art <t246370@st.yamagata-u.ac.jp>
2025-10-24 19:19:49 +0900

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <float.h>
+#include <limits.h>
+
+int main()
+{
+    printf("double型の最大値：%g\n",DBL_MAX);
+    printf("double型の最小値：%g\n",DBL_MIN);
+    printf("double型の精度：%d\n",DBL_DIG);
+    printf("unsined int型の最大値：%u\n",UINT_MAX);
+    printf("int型の最大値：%d\n",INT_MAX);
+    printf("int型の最小値：%d\n",INT_MIN);
+    printf("unsined long型の最大値：%Lu\n",ULONG_MAX);
+    printf("long型の最大値：%ld\n",LONG_MAX);
+    printf("long型の最小値：%ld\n",LONG_MIN);
+    printf("float型の最大値：%g\n",FLT_MAX);
+    printf("float型の最小値：%g\n",FLT_MIN);
+    printf("float型の精度：%d\n",FLT_DIG);
+    printf("long double型の最大値：%Le\n",LDBL_MAX);
+    printf("long double型の最小値：%Le\n",LDBL_MIN);
+    printf("long double型の精度：%d\n",LDBL_DIG);
+    printf("unsigned char型の最大値：%d\n",UCHAR_MAX);
+    printf("char型の最大値：%d\n",CHAR_MAX);
+    printf("char型の最小値：%d\n",CHAR_MIN);
+    printf("unsined short型の最大値：%d\n",USHRT_MAX);
+    printf("short型の最大値：%d\n",SHRT_MAX);
+    printf("short型の最小値：%d\n",SHRT_MIN);
+    
+    
+
+    return 0;
+}
